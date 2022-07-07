3d1b2fb72407401b6dfac0256c0d90caf9528c9b
Zheyuan Yang <andrewzheyuan.yang@mail.utoronto.ca>
2022-07-07 19:58:49 +0800

    Implement a simple LSTM.

--- main_architecture.py
+++ main_architecture.py
@@ -1,1 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+
+
 # LSTM model
+class LSTM_news_classifier(nn.Module):
+    def __init__(self, input_size, hidden_size, num_class):
+        super(LSTM_news_classifier, self).__init__()
+        self.hidden_size = hidden_size
+        self.rnn = nn.LSTM(input_size=input_size, hidden_size=hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_class)
+
+    def forward(self, x):
+        h0 = torch.zeros(1, x.size(0), self.hidden_size)
+        out, (h_n, c_n) = self.rnn(x, h0)
+        return self.fc(out[:,-1,:])
