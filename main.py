ec289c02cf9112c3887e6a478d086bfe54bbfc3f
Zheyuan Yang <andrewzheyuan.yang@mail.utoronto.ca>
2022-07-09 10:28:37 +0800

    First training with 100 epochs. See csv file for more information.

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ use_cuda = True
 News_LSTM = main_architecture.LSTM_news_classifier(50, 64, 7)
 
 train_loader, val_loader, test_loader = data_loader_function.data_loader()
-train_test_model.train_net(News_LSTM, 128, 0.01, 2, 0.9, train_loader, val_loader, 'July_8_8_33')
+train_test_model.train_net(News_LSTM, 128, 0.01, 100, 0.9, train_loader, val_loader, 'July_8_8_33')
