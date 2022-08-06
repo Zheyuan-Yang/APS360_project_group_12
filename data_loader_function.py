154221cf1cbed9f4a4841397eec07e9a6e69a148
Daixin Tian <105831455+AlezHibali@users.noreply.github.com>
2022-08-06 11:32:29 -0400

    make new_dataset as return value

--- data_loader_function.py
+++ data_loader_function.py
@@ -11,6 +11,7 @@ def data_loader(batch_size=128):
     train_data = pd.read_csv('./content/inshort_news_data-train.csv', header=0, encoding='ISO-8859-1', usecols=fields, skip_blank_lines=True)
     val_data = pd.read_csv('./content/inshort_news_data-val.csv', header=0, encoding='ISO-8859-1', usecols=fields, skip_blank_lines=True)
     test_data = pd.read_csv('./content/inshort_news_data-test.csv', header=0, encoding='ISO-8859-1', usecols=fields, skip_blank_lines=True)
+    new_data = pd.read_csv('./content/new_news_articles.csv', header=0, encoding='ISO-8859-1', usecols=fields, skip_blank_lines=True)
 
     # Creating training and testing data
     X_train = train_data['news_article']
@@ -21,6 +22,9 @@ def data_loader(batch_size=128):
 
     X_val = val_data['news_article']
     Y_val = val_data['news_category']
+    
+    X_new = new_data['news_article']
+    Y_new = new_data['news_category']
 
     for i in range(X_train.shape[0]):
       X_train[i] = X_train[i].split()
@@ -30,10 +34,14 @@ def data_loader(batch_size=128):
 
     for k in range(X_test.shape[0]):
       X_test[k] = X_test[k].split()
+    
+    for m in range(X_new.shape[0]):
+      X_new[k] = X_new[k].split()
         
     Y_train = pd.get_dummies(Y_train).to_numpy()
     Y_val = pd.get_dummies(Y_val).to_numpy()
     Y_test = pd.get_dummies(Y_test).to_numpy()
+    Y_new = pd.get_dummies(Y_new).to_numpy()
 
     # stopwords to eliminate useless words
     stopwords = []
@@ -52,6 +60,9 @@ def data_loader(batch_size=128):
     for ix in X_test:
       if (len(ix) > 61):
         ix = ix[0:61]
+    for ix in X_new:
+      if (len(ix) > 61):
+        ix = ix[0:61]
     
     # utilize Glove6B for embedding
     glove = torchtext.vocab.GloVe(name='6B', dim=50)
@@ -60,6 +71,7 @@ def data_loader(batch_size=128):
     embedding_matrix_train = np.zeros((X_train.shape[0], 61, 50))
     embedding_matrix_val = np.zeros((X_val.shape[0], 61, 50))
     embedding_matrix_test = np.zeros((X_test.shape[0], 61, 50))
+    embedding_matrix_new = np.zeros((X_new.shape[0], 61, 50))
 
     for i in range(X_train.shape[0]):
       for j in range(len(X_train[i])):
@@ -75,6 +87,11 @@ def data_loader(batch_size=128):
       for j in range(len(X_test[i])):
         if not (X_test[i][j].lower() in stopwords):
           embedding_matrix_test[i][j] = glove[X_test[i][j].lower()]
+        
+    for i in range(X_new.shape[0]):
+      for j in range(len(X_new[i])):
+        if not (X_new[i][j].lower() in stopwords):
+          embedding_matrix_new[i][j] = glove[X_new[i][j].lower()]
 
     X_train_t = torch.from_numpy(embedding_matrix_train).to(torch.float32)
     Y_train_t = torch.from_numpy(Y_train).to(torch.float32)
@@ -82,13 +99,17 @@ def data_loader(batch_size=128):
     Y_val_t = torch.from_numpy(Y_val).to(torch.float32)
     X_test_t = torch.from_numpy(embedding_matrix_test).to(torch.float32)
     Y_test_t = torch.from_numpy(Y_test).to(torch.float32)
+    X_new_t = torch.from_numpy(embedding_matrix_new).to(torch.float32)
+    Y_new_t = torch.from_numpy(Y_new).to(torch.float32)
 
     train_dataset = TensorDataset(X_train_t, Y_train_t)
     val_dataset = TensorDataset(X_val_t, Y_val_t)
     test_dataset = TensorDataset(X_test_t, Y_test_t)
+    new_dataset = TensorDataset(X_new_t, Y_new_t)
 
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size)
+    new_dataloader = DataLoader(new_dataset, batch_size=batch_size)
 
-    return train_dataloader, val_dataloader, test_dataloader
+    return train_dataloader, val_dataloader, test_dataloader, new_dataloader
