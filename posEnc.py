0c1c9f0f800e131c2e6a443b2082d6d2b8bb0782
PLAdam0319 <peixuanli0319@gmail.com>
2022-08-12 22:41:19 -0400

    Create posEnc.py
    add 1d position encoding function to try to minimize training time

---
+++ posEnc.py
@@ -0,0 +1,24 @@
+import torch
+
+
+def encoding_1d(pos, dim):
+    vec = torch.arange(pos)
+
+    if dim % 2 != 0:
+        return
+
+    emb = torch.zeros(vec.numel(), dim, dtype=torch.float)
+
+    itr = torch.arange(dim/2, dtype= torch.float)
+    itr /= dim/2.
+    itr = 1. / (10000 ** itr)
+
+    out = vec[:, None] @ itr[None,:]
+
+    eSin = torch.sin(out)
+    eCos = torch.cos(out)
+
+    emb[:, 0::2] = eSin
+    emb[:, 1::2] = eCos
+
+    return emb
